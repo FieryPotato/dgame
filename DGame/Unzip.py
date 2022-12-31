d1bfcf41dc5fda507d459e211e24f7b4936094a3
FieryPotato <65465835+FieryPotato@users.noreply.github.com>
2022-12-30 19:18:23 -0500

    Unzip.get_unzipper returns correct unzipper type for .zip & directories

--- DGame/Unzip.py
+++ DGame/Unzip.py
@@ -5,10 +5,33 @@ from typing import Protocol
 class Unzipper(Protocol):
     src: Path
 
-    def unzip(self, dst) -> None:
+    def unzip(self, dst: Path) -> None:
         """Unzip self.src to self.dst"""
         ...
 
 
 def get_unzipper(src: Path) -> Unzipper:
-    pass
+    file_types = {
+        '.zip': ZipUnzipper
+    }
+    if (suffix := src.suffix) in file_types:
+        unzipper = file_types[suffix]
+    else:
+        unzipper = DirUnzipper
+    return unzipper(src=src)
+
+
+class ZipUnzipper:
+    def __init__(self, src: Path):
+        self.src = src
+
+    def unzip(self, dst: Path) -> None:
+        return
+
+
+class DirUnzipper:
+    def __init__(self, src: Path):
+        self.src = src
+
+    def unzip(self, dst: Path) -> None:
+        return
--- test/test_Unzip.py
+++ test/test_Unzip.py
@@ -1,2 +1,17 @@
-class TestUnzipper:
-    pass
+from pathlib import Path
+
+import Unzip
+
+
+class TestUnzipFunctions:
+    def test_get_unzipper_for_zip(self) -> None:
+        path = Path('anything.zip')
+        actual = Unzip.get_unzipper(src=path)
+        assert type(actual) == Unzip.ZipUnzipper
+        assert actual.src == path
+
+    def test_get_unzipper_for_dir(self) -> None:
+        path = Path('path/to/whatever')
+        actual = Unzip.get_unzipper(src=path)
+        assert type(actual) == Unzip.DirUnzipper
+        assert actual.src == path
